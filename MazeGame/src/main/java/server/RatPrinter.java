9455c1dac3e21e2f8719e891ce1f9743ddb2e542
Khanathan <dgk25500@gmail.com>
2025-08-08 01:28:18 -0700

    Took rat printer out to a new util class

---
+++ MazeGame/src/main/java/server/RatPrinter.java
@@ -0,0 +1,70 @@
+package server;
+
+public class RatPrinter {
+    public static void printRat() {
+
+        System.out.println(
+                "                                   .;+;                                                                                                                                     ");
+        System.out.println(
+                "                                  ;:::+                                                                                                                                     ");
+        System.out.println(
+                "                                 ;::.:                         ::;+;++x+::                                                                                                  ");
+        System.out.println(
+                "                                X$$X++::                  ;XXXXxxxxx+++++xX$$x                                                                                              ");
+        System.out.println(
+                "                            ++xxxxxx++;;;++;.      ;x&$$&&&$XXXXxXXX+;++;+xXX$$XX                                                                                           ");
+        System.out.println(
+                "                      +$$XX$XX$XXXXX$;;;;+xxXx+xxxxXX$$X$$$$X$XX+;+++x++++++xXX$$$$.                                                                                        ");
+        System.out.println(
+                "                .  X$X$$$XXXXXXXXX$$x:;++X+x++++x+xXxxX$$XXXXXXXXXX+Xx++++xxXxXXXX$$X                                                                                       ");
+        System.out.println(
+                "                .$XXXXXXXX+XXXXxxx$Xx;;++XXx+++x++x+XxXX$Xx+XXX$X$$XXxxxx++XXxXXX$$$X$;                                                                                     ");
+        System.out.println(
+                "             .+++XXXXXx+&&&&++XXXx++++X+xx++xxxxXxxxx$$$$XXX$X$X$$$XXXx+++++xXXXXXX$$$$;                                                                                    ");
+        System.out.println(
+                "           :+++++++XXX+XXx+;XXXXX+++x;.:+xXXxxxXxXXX$$$$$$$$$$$$$X$XxXx++++xxXXx$$$$$$$$+                                                                                   ");
+        System.out.println(
+                "            :+XXx+;+X$$$$$XXX$$&XXxxxxXX$$$$$XXXXX$X$$$$$$X$$X$X$$XXXXxxx;++x+++XxX$$$$X$x                                                                                  ");
+        System.out.println(
+                "             ;+xXx++xX$$$$$$XXX$$XXXxxXX$$$$$$$XXXXX$$$$X$X$$$$$$$XXX$x+++x++x++XxXXXXXXX$X                                                                                 ");
+        System.out.println(
+                "               ++++XxXxX$$XXXXXXXXXXXXX$$$$$$X$XXX$$$XXX$XXX$$XXXX+xx+xXxx++++++XX+++XX$XXX+                                                                                ");
+        System.out.println(
+                "                   .:;;+++xx+xXXXXXXX$X$$$X$XX$XxX$X$$$$X$$XXX$$$XXxXXXXXx++xx+xxx++XX$X$XXx                                                                                ");
+        System.out.println(
+                "                              :xxXXXXX$XXXXxXXXXX+XXXX$$$$XXXXX$XXXXXXXXxxx++++x++x++XXXXXXX:                                                                               ");
+        System.out.println(
+                "                                :xxXXXXXX$XXXXxXXXX+X$$$$$x$$XXXXxx$XXxxX+;+++++x+x++XXXXXXx.                                                                               ");
+        System.out.println(
+                "                                  ;xxXXXXXXXxXxx+XXxXXXXXXXxXxxxxxxXX+X+xxxxx+++++;x++X$XXXx.                                  .:;;;;++;;;::..                              ");
+        System.out.println(
+                "                                   :++xXXxxXxxxxX+xxxxX$XXXxxxxxxXX+++xX++++x++++++;x+xXXXXXx:                      :;;;::..:::::::;;;;;;;;;;;;;::;;+.                      ");
+        System.out.println(
+                "                                   .x+xxXxXXXxx+++x++xXXXXxXxxxxXxXxxxxxxx+xxX+x;+xx+x+XXXXXXXx+.       ..:;;;:.:::::::::::::::;+;;:.      .:;+x++;;;;::;;                  ");
+        System.out.println(
+                "                                   $XX++x++++x++++xxxxxXXXxXx+++xXxxxxxX+++++xxx;++x+xXxXXXxxx+x::::::::;:::;:;;;::::;;;;.                           .;+;;;;+               ");
+        System.out.println(
+                "                                  ++xX:;++++++++++xx+xxx+x+++xx+xx+xx++xx+xxxx+x+X++++xxXxxxx+++;:.::::::::::;++:                                         :+:.::            ");
+        System.out.println(
+                "                                 ++++; .++++++x+xx+xx++x++++;+++++++++++x++X+x++Xx+++++xxxx+xxxxxxx++;:.                                                     .;:::.         ");
+        System.out.println(
+                "                                :++:....:+++x++++;;;;;+;++;+++++++++++++;;;:;++xx++xxxxX+x+;.                                                                    ::::       ");
+        System.out.println(
+                "                          . ....:+;:::::;x++;;;;;;;++xX+x+:::;;++++;;;:. .:::+XXxxxXxx+++:                                                                           .      ");
+        System.out.println(
+                "                    .......:::::;;;;;;+++x+++;;;;+xxxXxXxx+x+;;:::.......:;+xXXXXxxx+++:.                                                                                   ");
+        System.out.println(
+                "               ......:::::::;;;+++++xxxxxx+++;;;xxxxxxxx+++++++++++++xxxx+XXXX$$X$;+:::.                                                                                    ");
+        System.out.println(
+                "            ......::::::::;;;;++xxXXXXXx+++;;;xx++++++++++;;;;;;;;;;;;;;;+++xxXXx:.  :                                                                                      ");
+        System.out.println(
+                "            ........:::::::::::;;;;++++:...:+++;;;:::::::::::::::.........:...                                                                                              ");
+        System.out.println(
+                "                  . ............:::::;:.   ;;::...                        .     .                                                                                           ");
+        System.out.println(
+                "                                  ..::.. .:.                           .....  :  .                                                                                          ");
+        System.out.println("                                .. .. .::     ");
+        System.out.println("                                   .                    ");
+
+    }
+}
